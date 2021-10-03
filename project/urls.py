5cd4cfc0de3b20e4a2f5bcbe0e2816e76fb7b624
exit <exit>
2021-10-03 14:31:36 +0200

    Add About and FAQ views

--- project/urls.py
+++ project/urls.py
@@ -20,9 +20,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('settings.urls'), name='home'),
     path('property/', include('property.urls'), name='property'),
     path('blog/', include('blog.urls'), name='blog'),
+    path('about/', include('about.urls'), name='about'),
+    path('', include('settings.urls'), name='home'),
     path('summernote/', include('django_summernote.urls')),
 ]
 
